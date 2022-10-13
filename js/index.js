5e67f3c0a8c5ac1093247530592f03a1df14a79a
Vikasyadav25 <vikassinghyadav6@gmail.com>
2022-10-13 17:37:59 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// var age=14;
+
+// if(age>18){
+//     console.log("you are eligible for votes")
+// }
+// else if (age>11) {
+//     console.log("you are not eligible for votes")
+
+// }
+// else{
+//     console.log("you are a kid")
+// alert("hello world ")
+// console.log ("vikas yadav ")
+// console.assert (4==60)
+// vikas yadav form gorakhpur 
